0d1ca2ec0485f57d8b864e5919c74669b123ab6a
Jason Adam <22adamj22@gmail.com>
2021-01-13 21:02:18 -0700

    added stub for 0108

---
+++ src/chapter_01/problem_0108.py
@@ -0,0 +1,9 @@
+# Zero Matrix:
+# Write an algorithm such that if an element in an M x N
+# matrix is 0, it's entire row and column are set to 0.
+
+from typing import List
+
+
+def zero_matrix(matrix: List[List[int]]) -> None:
+    pass
